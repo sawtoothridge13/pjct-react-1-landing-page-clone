c76dbd531038853b6e872e5a882259e17e07be70
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-10 14:52:01 +0200

    Add basic outline structure to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import './App.css';
-import logo from './logo.svg';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div>
+          <span>Bolton Remote is now a part of SupportNinja!</span>
+          <a href="/#">Read about the acquisition.</a>
+        </div>
+
+        <nav>
+          <img />
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+
+      <main>
+        <section>
+          <h1>A better way to grow</h1>
+          <p>
+            Seamlessly scale your team and flourish with global outsourced
+            talent.
+          </p>
+        </section>
+        <section>Section 2</section>
+        <section>Section 3</section>
+        <section>Section 4</section>
+        <section>Section 5</section>
+        <section>Section 6</section>
+        <section>Section 7</section>
+      </main>
+      <footer>footer</footer>
+    </>
   );
 }
